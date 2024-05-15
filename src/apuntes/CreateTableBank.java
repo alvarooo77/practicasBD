c8b72c50fe3ab1b87c561a3fbae63e1fdbc42c40
alvarooo77 <alvaro.s376781@cesurformacion.com>
2024-05-15 12:36:23 +0200

    usando loader actual

--- src/apuntes/CreateTableBank.java
+++ src/apuntes/CreateTableBank.java
@@ -8,7 +8,7 @@ import java.sql.Statement;
 
 public class CreateTableBank {
    // Attributes
-   String driver = "com.mysql.jdbc.Driver";
+   String driver = "com.mysql.cj.jdbc.Driver";
    String url = "jdbc:mysql://localhost:3306/bank";
    String login = "root";
    String password = "";
